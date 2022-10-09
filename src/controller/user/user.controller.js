9dc1cf4e1e12dcbf41d3c789b4a6196137884820
xuantien <taxuantien312000@gmail.com>
2022-10-09 17:10:22 +0700

    commit

--- src/controller/user/user.controller.js
+++ src/controller/user/user.controller.js
@@ -4,10 +4,10 @@ module.exports = UserController = {
   registerUser: async (req, res) => {
     const isExist = await userModel.findOne({ email: req.body.email });
     if (!isExist) {
-      const passwordHash = await bcrypt.hash(req.body.password, 10);
+      // const passwordHash = await bcrypt.hash(req.body.password, 10);
       const newUser = await userModel.create({
         ...req.body,
-        password: passwordHash,
+        // password: passwordHash,
       });
       return res.status(200).json(newUser);
     } else {
@@ -17,11 +17,11 @@ module.exports = UserController = {
   loginUser: async (req, res) => {
     const existUser = await userModel.findOne({ email: req.body.email });
     if (existUser) {
-      const mathPassword = await bcrypt.compare(
-        req.body.password,
-        existUser.password
-      );
-      if (!mathPassword) {
+      // const mathPassword = await bcrypt.compare(
+      //   req.body.password,
+      //   existUser.password
+      // );
+      if (req.body.password === existUser.password) {
         return res.status(400).json({ message: "Mật khẩu không đúng!" });
       } else {
         return res.status(200).json({ message: "Thành công" });
