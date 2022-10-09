a1e5b322b95a6ca761b25538ffd298b09f082bcf
xuantien <taxuantien312000@gmail.com>
2022-10-09 17:04:21 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api", userRoute);
-app.use("/api", categoryRoute);
+app.use("/api", categoryRoute); 
 app.use("/api", productRoute);
 connect.connect();
 
